9bf1c118df035f339e8871d7e0089de7157b209f
Yash Raghubanshi <yashraghubanshi@Yashs-MacBook-Pro-3.local>
2025-02-12 22:40:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML === '=') {
+            try{
+                string = eval(string);
+                input.value = string;
+            }catch(error){
+                input.value = 'Error';
+            }
+        }
+        else if(e.target.innerHTML === 'AC'){
+            string = "";
+            input.value = string;
+        }
+        
+    });
+});
